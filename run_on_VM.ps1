8706a6cb35f21e59a98be30c6d39cee17a76ab1a
lanajahshan1 <137297023+lanajahshan1@users.noreply.github.com>
2023-06-22 13:19:28 +0300

    Delete run_on_VM.ps1

--- run_on_VM.ps1
+++
@@ -1,4 +0,0 @@
-Get-AzVM -ResourceGroupName "Candidates_Homeworks_Lana" -Name "simple-vm"
-
-Invoke-AzVMRunCommand -ResourceGroupName 'Candidates_Homeworks_Lana' -Name 'simple-vm' 
- -CommandId 'RunPowerShellScript' -ScriptPath '.\working_with_blobs.ps1' 
