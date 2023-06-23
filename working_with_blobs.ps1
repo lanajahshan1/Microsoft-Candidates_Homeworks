617c1f2112ce4c5ebbedc78733075535c3f64ab0
lanajahshan1 <137297023+lanajahshan1@users.noreply.github.com>
2023-06-23 13:21:54 +0300

    Add files via upload

--- working_with_blobs.ps1
+++ working_with_blobs.ps1
@@ -24,20 +24,10 @@ for ($i = 1; $i -le 100; $i++) {
   $blobName = "file$i.txt"
 
     New-Item -Path . -Name $blobName -ItemType "file" -Value "This is a blob." -Force
-    $blob = Set-AzStorageBlobContent -Container $sourceContainerName -File $blobName -Blob $blobName -Context $sourceContext -Force
+    Set-AzStorageBlobContent -Container $sourceContainerName -File $blobName -Blob $blobName -Context $sourceContext -Force
     Remove-Item -Path "file$i.txt" -Force
-}
+    Start-AzStorageBlobCopy -SrcContainer $sourceContainerName -SrcBlob $blobName -DestContainer $destinationContainerName -DestBlob $blobName -Context $sourceContext -DestContext $destinationContext
 
-for ($i = 1; $i -le 100; $i++) {
-  $blobName = "file$i.txt"
-
-  $copyStatus = Start-AzStorageBlobCopy -SrcContainer $sourceContainerName -SrcBlob $blobName -DestContainer $destinationContainerName -DestBlob $blobName -Context $sourceContext -DestContext $destinationContext
-
-  if ($copyStatus) {
-      Write-Host "Blob '$blobName' copied to destination storage account successfully."
-  } else {
-      Write-Host "Failed to copy blob '$blobName' to destination storage account."
-  }
 }
 
 
