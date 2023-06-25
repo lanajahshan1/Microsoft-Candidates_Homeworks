dcaf62d00f6dd58a048d92aa5be3f2d1f4f23c4c
lanajahshan1 <137297023+lanajahshan1@users.noreply.github.com>
2023-06-25 21:21:33 +0300

    Add files via upload

--- run_on_VM.ps1
+++ run_on_VM.ps1
@@ -1,3 +1,3 @@
-Get-AzVM -ResourceGroupName "test1" -Name "simple-vm"
+Get-AzVM -ResourceGroupName "test" -Name "simple-vm"
 
-Invoke-AzVMRunCommand -ResourceGroupName 'test1' -Name 'simple-vm' -CommandId 'RunPowerShellScript' -ScriptPath 'working_with_blobs.ps1' 
+Invoke-AzVMRunCommand -ResourceGroupName 'test' -Name 'simple-vm' -CommandId 'RunPowerShellScript' -ScriptPath 'working_with_blobs.ps1' 
