d00ea540541192cbc85d0e08b3dbdc35730fbf56
lanajahshan1 <137297023+lanajahshan1@users.noreply.github.com>
2023-06-26 10:04:13 +0300

    Add files via upload

--- run_on_VM.ps1
+++ run_on_VM.ps1
@@ -1,2 +1,3 @@
-Get-AzVM -ResourceGroupName "test" -Name "simple-vm"
-Invoke-AzVMRunCommand -ResourceGroupName 'test' -Name 'simple-vm' -CommandId 'RunPowerShellScript' -ScriptPath 'working_with_blobs.ps1' 
+Get-AzVM -ResourceGroupName "test1" -Name "simple-vm"
+
+Invoke-AzVMRunCommand -ResourceGroupName 'test' -Name 'simple-vm' -CommandId 'RunPowerShellScript' -ScriptPath 'test_script.ps1' 
--- test_script.ps1
+++ test_script.ps1
@@ -1,1 +1,14 @@
-write-Output "run weeeeeeee"
+
+$sourceStorageAccountName = "lanastorageaccount11"
+$sourceContainerName = "sourceblobcontainer"
+
+$keyA = (Get-AzStorageAccountKey -ResourceGroupName "test" -Name $sourceStorageAccountName)[0].Value
+$sourceContext = New-AzStorageContext -StorageAccountName $sourceStorageAccountName -StorageAccountKey $keyA
+New-AzStorageContainer -Name $sourceContainerName -Context $sourceContext -Permission Container
+
+
+$blobName = "file$i.txt"
+
+New-Item -Path . -Name $blobName -ItemType "file" -Value "This is a blob." -Force
+Set-AzStorageBlobContent -Container $sourceContainerName -File $blobName -Blob $blobName -Context $sourceContext -Force
+Remove-Item -Path "file$i.txt" -Force
