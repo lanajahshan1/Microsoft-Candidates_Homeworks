b1d97e963ff7c45ef7af7534a4ef351caad36f27
lanajahshan1 <137297023+lanajahshan1@users.noreply.github.com>
2023-06-26 10:32:59 +0300

    Add files via upload

--- run_on_VM.ps1
+++ run_on_VM.ps1
@@ -1,6 +1,6 @@
-Get-AzVM -ResourceGroupName "test" -Name "simple-vm"
+Get-AzVM -ResourceGroupName "Candidates_Homework_Lana" -Name "simple-vm"
 
-$result = Invoke-AzVMRunCommand -ResourceGroupName 'test' -Name 'simple-vm' -CommandId 'RunPowerShellScript' -ScriptPath 'test_script.ps1' 
+$result = Invoke-AzVMRunCommand -ResourceGroupName 'Candidates_Homework_Lana' -Name 'simple-vm' -CommandId 'RunPowerShellScript' -ScriptPath 'test_script.ps1' 
 
 $output = $result.Value[0].Message
 Write-Output "Script Output: $output"
--- working_with_blobs.ps1
+++ working_with_blobs.ps1
@@ -2,17 +2,13 @@ if (-not (Get-Module -ListAvailable Az.Storage)) {
   Install-Module -Name Az.Storage -Force
 }
 
-Connect-AzAccount
-
-Set-AzContext -Subscription 75a700f9-db14-47c2-b97c-4481c0611f0b
-
 $sourceStorageAccountName = "lanastorageaccount11"
 $destinationStorageAccount = "lanastorageaccount22"
 $sourceContainerName = "sourceblobcontainer"
 $destinationContainerName = "destinationblobcontainer"
 
-$keyA = (Get-AzStorageAccountKey -ResourceGroupName "test" -Name $sourceStorageAccountName)[0].Value
-$keyB = (Get-AzStorageAccountKey -ResourceGroupName "test" -Name $destinationStorageAccount)[0].Value
+$keyA = (Get-AzStorageAccountKey -ResourceGroupName "Candidates_Homework_Lana" -Name $sourceStorageAccountName)[0].Value
+$keyB = (Get-AzStorageAccountKey -ResourceGroupName "Candidates_Homework_Lana" -Name $destinationStorageAccount)[0].Value
 
 $sourceContext = New-AzStorageContext -StorageAccountName $sourceStorageAccountName -StorageAccountKey $keyA
 $destinationContext = New-AzStorageContext -StorageAccountName $destinationStorageAccount -StorageAccountKey $keyB
@@ -24,26 +20,8 @@ for ($i = 1; $i -le 100; $i++) {
   $blobName = "file$i.txt"
 
     New-Item -Path . -Name $blobName -ItemType "file" -Value "This is a blob." -Force
-    $blob = Set-AzStorageBlobContent -Container $sourceContainerName -File $blobName -Blob $blobName -Context $sourceContext -Force
+    Set-AzStorageBlobContent -Container $sourceContainerName -File $blobName -Blob $blobName -Context $sourceContext -Force
     Remove-Item -Path "file$i.txt" -Force
-  if ($blob) {
-      Write-Host "Blob '$blobName' uploaded to source storage account successfully."
-  } else {
-      Write-Host "Failed to upload blob '$blobName' to source storage account."
-  }
+
+    Start-AzStorageBlobCopy -SrcContainer $sourceContainerName -SrcBlob $blobName -DestContainer $destinationContainerName -DestBlob $blobName -Context $sourceContext -DestContext $destinationContext
 }
-
-for ($i = 1; $i -le 100; $i++) {
-  $blobName = "file$i.txt"
-
-  $copyStatus = Start-AzStorageBlobCopy -SrcContainer $sourceContainerName -SrcBlob $blobName -DestContainer $destinationContainerName -DestBlob $blobName -Context $sourceContext -DestContext $destinationContext
-
-  if ($copyStatus) {
-      Write-Host "Blob '$blobName' copied to destination storage account successfully."
-  } else {
-      Write-Host "Failed to copy blob '$blobName' to destination storage account."
-  }
-}
-
-
-Disconnect-AzAccount
