c28756863914c893740850c6d779a69a11593638
lanajahshan1 <137297023+lanajahshan1@users.noreply.github.com>
2023-06-26 10:07:23 +0300

    Add files via upload

--- run_on_VM.ps1
+++ run_on_VM.ps1
@@ -1,3 +1,3 @@
-Get-AzVM -ResourceGroupName "test1" -Name "simple-vm"
+Get-AzVM -ResourceGroupName "test" -Name "simple-vm"
 
 Invoke-AzVMRunCommand -ResourceGroupName 'test' -Name 'simple-vm' -CommandId 'RunPowerShellScript' -ScriptPath 'test_script.ps1' 
